4ee5aa410503c3054c037125b6fb1fbd1c760728
Le Huy <lehuy@Les-MacBook-Air.local>
2023-05-08 11:37:27 +0700

    thuc hanh 1

---
+++ C0323G1_le_huy_module2/src/ss1_introduction_to_java/thuc_hanh/thuc_hanh_hien_thi_thoi_gian_he_thong.java
@@ -0,0 +1,8 @@
+package ss1_introduction_to_java.thuc_hanh;
+import java.util.Date;
+public class thuc_hanh_hien_thi_thoi_gian_he_thong {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println("Now is: " + date);
+    }
+}
