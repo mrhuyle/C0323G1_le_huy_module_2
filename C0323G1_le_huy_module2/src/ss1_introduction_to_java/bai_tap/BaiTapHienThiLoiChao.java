07a252f7f13a895996b8f26f39bbfbabf8c9ea8d
Le Huy <lehuy@Les-MacBook-Air.local>
2023-05-08 11:29:02 +0700

    bai tap hien thi loi chao

--- C0323G1_le_huy_module2/src/ss1_introduction_to_java/bai_tap/BaiTapHienThiLoiChao.java
+++ C0323G1_le_huy_module2/src/ss1_introduction_to_java/bai_tap/BaiTapHienThiLoiChao.java
@@ -1,6 +1,8 @@
+package ss1_introduction_to_java.bai_tap;
+
 import java.util.Scanner;
 
-public class Main {
+public class BaiTapHienThiLoiChao {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your name: ");
