139d4284635cfe14fc3a0485fbe70da1057ebb20
Le Huy <lehuy@Les-MacBook-Air.local>
2023-05-10 10:32:45 +0700

    ss2 sumofcollumn

---
+++ C0323G1_le_huy_module2/src/ss3_array_method/exercise/SumOfCollumArray.java
@@ -0,0 +1,38 @@
+package ss3_array_method.exercise;
+
+import java.util.Scanner;
+
+public class SumOfCollumArray {
+    public static void main(String[] args) {
+        //Make a array
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input a row and a collumn of a array");
+        System.out.println("Input row index of array: ");
+        int row = Integer.parseInt(scanner.nextLine());
+        System.out.println("Input collumn index of array: ");
+        int col = Integer.parseInt(scanner.nextLine());
+        double[][] arrDouble = new double[row][col];
+        for (int i = 0; i < arrDouble.length; i++) {
+            for (int j = 0; j < arrDouble[i].length; j++) {
+                System.out.println("Input the element that has index " + i + j);
+                arrDouble[i][j] = Double.parseDouble(scanner.nextLine());
+            }
+        }
+
+        //Input index collumn to sum:
+        System.out.println("Input a index of collumn you want to sum: ");
+        int index = Integer.parseInt(scanner.nextLine());
+
+        //Result:
+        System.out.println("Result: " + sumCol(arrDouble, index));
+    }
+
+    //Method use to sum values in one collumn:
+    public static double sumCol(double[][] arr, int index) {
+        double sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[index][i];
+        }
+        return sum;
+    }
+}
