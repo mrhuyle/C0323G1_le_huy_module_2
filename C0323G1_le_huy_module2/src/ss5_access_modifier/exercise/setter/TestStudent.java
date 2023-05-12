cb55aebbd7959f25bf4a40a2bafd7e39a35bc164
Le Huy <lehuy@Les-MacBook-Air.local>
2023-05-12 15:13:27 +0700

    ss5

--- C0323G1_le_huy_module2/src/ss5_access_modifier/exercise/setter/TestStudent.java
+++ C0323G1_le_huy_module2/src/ss5_access_modifier/exercise/setter/TestStudent.java
@@ -1,5 +1,7 @@
 package ss5_access_modifier.exercise.setter;
 
+import ss5_access_modifier.exercise.access_modifier.Circle;
+
 public class TestStudent {
     public static void main(String[] args) {
         Student studentDefault = new Student();
@@ -8,5 +10,6 @@ public class TestStudent {
         student1.setName("Huy");
         student1.setClasses("C0323G1");
         System.out.println(student1.toString());
+        Circle circle = new Circle();
     }
 }
