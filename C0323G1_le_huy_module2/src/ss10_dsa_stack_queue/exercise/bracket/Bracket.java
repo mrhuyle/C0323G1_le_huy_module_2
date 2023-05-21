fd33bd0958aee76813566942ef97dfe24e36c98f
Le Huy <lehuy@Les-MacBook-Air.local>
2023-05-21 10:43:09 +0700

    ss10 bracket 2

--- C0323G1_le_huy_module2/src/ss10_dsa_stack_queue/exercise/bracket/Bracket.java
+++ C0323G1_le_huy_module2/src/ss10_dsa_stack_queue/exercise/bracket/Bracket.java
@@ -2,10 +2,22 @@ package ss10_dsa_stack_queue.exercise.bracket;
 
 import java.util.Stack;
 
+/**
+ * Author: Le Huy
+ * Description: Using Stack to check the function with brackets
+ * Method: checkBracket(String str) has param String (function) and return datatype boolean
+ * Notice: check the case that the function doesn't contain any "(" or ")" symbol.
+ */
 public class Bracket {
     public static void main(String[] args) {
-        String str = "s * (s – a) * s – b) * (s – c)";
-        System.out.println(checkBracket(str));
+        String str = "(– b + (b^2 – 4*a*c)^(0.5/ 2*a))";
+        if (!str.contains("(") && !str.contains(")")) {
+            System.out.println("The function: " + str + " does not contain any brackets");
+        } else if (checkBracket(str)) {
+            System.out.println("The function: " + str + " contains brackets and they are TRUE");
+        } else {
+            System.out.println("The function: " + str + " contains brackets and they are FALSE");
+        }
     }
 
     public static boolean checkBracket(String str) {
