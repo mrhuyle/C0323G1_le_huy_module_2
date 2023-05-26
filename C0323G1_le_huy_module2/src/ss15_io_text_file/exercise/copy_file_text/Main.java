c71d59c0f71b1f63b200639c462ea2d2bf3911e9
Le Huy <lehuy@Les-MacBook-Air.local>
2023-05-26 11:44:00 +0700

    ss15

--- C0323G1_le_huy_module2/src/ss15_io_text_file/exercise/copy_file_text/Main.java
+++ C0323G1_le_huy_module2/src/ss15_io_text_file/exercise/copy_file_text/Main.java
@@ -5,14 +5,16 @@ import java.util.List;
 
 /**
  * Author: Le Huy
- * Description: Practice input stream and output stream with copy and
+ * Description: Practice input stream and output stream by reading data from an existing file (test.txt) and writing it to a new file.
+ * Notice: Diplay number of characters from a string in an existing file.
+ * Classes related: ReadAndWriteFileText class have 2 methods: readFileText(String filePath) and writeFileText(String filePath, List<Character> characterList)
  */
 public class Main {
     public static void main(String[] args) {
         //Read file test.txt in package:
         List<Character> characters = new ArrayList<>();
         ReadAndWriteFileText readAndWriteFileText = new ReadAndWriteFileText();
-        characters = readAndWriteFileText.readFileText("src/ss15_io_text_file/exercise/copy_file_text/test.text");
+        characters = readAndWriteFileText.readFileText("src/ss15_io_text_file/exercise/copy_file_text/test.txt");
         System.out.println("Number of characters in test.txt file is (details in new.txt file) : " + readAndWriteFileText.writeFileText("src/ss15_io_text_file/exercise/copy_file_text/new.txt", characters));
     }
 }
