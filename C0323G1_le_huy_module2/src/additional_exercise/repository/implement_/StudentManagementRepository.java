532095cdd9409edd4ad3fb7dc9fc7bef61032138
Le Huy <lehuy@Les-MacBook-Air.local>
2023-05-29 15:37:46 +0700

    mvc add

--- C0323G1_le_huy_module2/src/additional_exercise/repository/implement_/StudentManagementRepository.java
+++ C0323G1_le_huy_module2/src/additional_exercise/repository/implement_/StudentManagementRepository.java
@@ -12,12 +12,6 @@ import java.util.regex.Pattern;
 public class StudentManagementRepository implements IStudentManagementRepository {
     private static List<Person> personList = new ArrayList<>();
 
-//    static {
-//        personList.add(new Student("S001", "Le Huy", "20/06/1989", true, "C0323G1", 9.5f));
-//        personList.add(new Student("S002", "Nguyen Thi B", "01/01/1999", false, "C0423G1", 8.5f));
-//        personList.add(new Teacher("T003", "Louis Le", "20/06/1979", true, "Java"));
-//    }
-
     @Override
     public List<Person> getAll() {
         personList = ReadWriteFileCsv.readFileCsv();
